e2d6136d9ae2d58192bcec08ce107a6afcd6edad
YangTaeWook <didxodnr0769@gmail.com>
2024-03-28 11:33:42 +0900

    백그라운드에서 포그라운드로 전환 되었을 때 리스트 갱신 요청 추가

--- round_05_menu_archive/01_project/00_web/src/components/ContentsContainer.jsx
+++ round_05_menu_archive/01_project/00_web/src/components/ContentsContainer.jsx
@@ -8,11 +8,26 @@ import { getMenuList } from "../system/api";
 const ContentsContainer = () => {
   const [contents, setContents] = useState([]);
   useEffect(() => {
-    // 현재 리스트 API 미구현
+    document.addEventListener("visibilitychange", function () {
+      if (document.visibilityState === "visible") {
+        getData();
+        // 필요한 작업 수행
+      } else {
+        console.log("웹 애플리케이션이 백그라운드로 전환되었습니다.");
+      }
+    });
+
+    getData();
+  }, []);
+
+  /**
+   * 메뉴 목록 조회
+   */
+  const getData = () => {
     getMenuList().then((res) => {
       setContents(res.data || []);
     });
-  }, []);
+  };
 
   return (
     <div className="contents-container">
